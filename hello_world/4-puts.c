371c4ad2d6f6f53c7edada224a4e9fe00d2be9ed
scotty800 <scottyndanga02400@gmail.com>
2024-06-17 13:37:27 +0000

    Use the function puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-int main(void) {
-	
-	/*display the programme*/
-	puts ( "Programming is like building a multilingual puzzle" );
-	
-return 0;
+int main(void)
+{
+/*display the programme*/
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }

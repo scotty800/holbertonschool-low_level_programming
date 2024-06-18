39ca2e562446e7b452371e43f416b8a51e5672af
scotty800 <scottyndanga02400@gmail.com>
2024-06-18 16:02:31 +0000

    test5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Return: Allways 0 (Succes)
+ */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int numb = 0;
+
+	while (numb <= 10)
+	{
+	printf("%d\n", numb);
+	numb++;
+	}
+	return (0);
+}

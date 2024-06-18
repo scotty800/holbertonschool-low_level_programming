6d7191856a79ffc1effd720e08e811a0e09d0129
scotty800 <scottyndanga02400@gmail.com>
2024-06-18 13:51:07 +0000

    test3

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Return: Allways 0 (Succes)
+ */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha < 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	printf("\n");
+	return (0);
+}

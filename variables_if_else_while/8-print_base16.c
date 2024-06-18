73ca237a32c65b2cf4c36df6eb974748e885f378
scotty800 <scottyndanga02400@gmail.com>
2024-06-18 18:54:29 +0000

    test5

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -14,12 +14,12 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num < 9; num++)
 	{
 		putchar(num + '0');
 	}
 
-	for (num = 0; num <= 6; num++)
+	for (num = 0; num < 6; num++)
 	{
 		putchar(num + 'a');
 	}

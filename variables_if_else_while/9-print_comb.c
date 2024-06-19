2f50afa5111a4eddac43b6a416dfab5861c11e14
scotty800 <scottyndanga800@gmail.com>
2024-06-19 12:02:25 +0000

    test6

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -15,14 +15,16 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		continue;
 
 		if (num == '9')
 		{
+		continue;
+
 		putchar(',');
 		putchar(' ');
 		}
 		putchar('\n');
 	}
 		return (0);
+
 }

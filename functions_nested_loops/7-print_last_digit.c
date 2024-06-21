355a4b3003d51cc3317212bb3a15d82465aa658b
scotty800 <scottyndanga800@gmail.com>
2024-06-21 12:55:16 +0000

    test

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,21 @@
 /**
  * print_last_digit -verfi si int vas me retourner une valeur %
  * @num : variable entier
+ * lastN : variable entiter
  * Return: % de la variable en positive ou negation
  */
 
 int print_last_digit(int num)
 {
-	if (num >= 0)
+	int lastN = num % 10;
+
+	if (lastN < 0)
 	{
-		return (num % 10);
+		lastN = -lastN;
 	}
-	else
-		return (-num % 10);
+
+	_putchar (lastN + '0');
+
+	return (lastN);
+
 }

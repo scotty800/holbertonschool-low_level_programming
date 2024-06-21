d05eda225ce8c3b3c1a5a5727a58518888719071
scotty800 <scottyndanga02400@gmail.com>
2024-06-21 00:23:40 +0000

    test8

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet_x10 -display l'alphabet de a jusqu'a z 10fois
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int time;
+
+	for (time = 1; time <= 10; time++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+	}
+	_putchar('\n');
+}

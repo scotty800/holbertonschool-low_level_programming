30203b9913336a9fa87dc4a07fb87ea8cb38349f
scotty800 <scottyndanga800@gmail.com>
2024-06-21 14:42:15 +0000

    test

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @num1 : name de variable int
+ * @num2 : name de variable int
+ * Return: returns the result
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}

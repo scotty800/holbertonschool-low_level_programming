0503fafd7f3d19a8721f496e4f685e0bc88f76ab
scott <scotty>
2024-06-23 16:02:02 +0000

    test10

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,9 @@ void print_to_98(int n)
 {
 	int lastn = n;
 
-	if (n <= 98 && n >= 98)
+	if (n <= 98)
 	{
-		while (lastn <= 98 && lastn >= 98)
+		while (lastn <= 98)
 		{
 			if (lastn != 98)
 			{
@@ -33,7 +33,24 @@ void print_to_98(int n)
 	}
 	else
 	{
-		lastn--;
+		while (lastn >= 98)
+		{
+			if (lastn != 98)
+			{
+				_putchar(' ');
+				_putchar(',');
+			}
+			if (lastn >= 10)
+			{
+				_putchar((lastn / 10) + '0');
+				_putchar((lastn % 10) + '0');
+			}
+			else
+			{
+				_putchar(lastn + '0');
+			}
+			lastn--;
+		}
 	}
 	_putchar('\n');
 

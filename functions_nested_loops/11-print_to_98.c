f49a60b1d323bed17428df6b9b99d36fda56a6d0
scott <scotty>
2024-06-23 19:27:34 +0000

    test10

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,16 +10,19 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n < 98; n++)
+		while (n < 98)
 		{
-			printf("%d\n, ", n);
+			printf("%d\n, ", n++);
+			printf("%d\n", n);
 		}
 	}
+
 	else
 	{
-		for (; n > 98; n--)
+		while (n > 98)
 		{
-			printf("%d, ", n);
+			printf("%d\n, ", n--);
+			printf("%d\n", n);
 		}
 	}
 }

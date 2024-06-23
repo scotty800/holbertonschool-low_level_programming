1875cf19d3de6ea5a6f50220915f48ebba1c63ea
scott <scotty>
2024-06-23 07:46:00 +0000

    test9

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - a en parametre un entier
+ * @n : name de variable int
+ */
+
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		for (n = 98; n > 0; n--)
+		{
+			putchar (' ');
+			putchar (',');
+		}
+	}
+
+}

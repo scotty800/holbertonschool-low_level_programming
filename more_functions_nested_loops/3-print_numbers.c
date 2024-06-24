3c6a0e1d895a4deeaa3652ef67581f213ce50321
s <s>
2024-06-24 12:26:30 +0000

    test11

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower -verifi si c est en min si oui return 1
+ *@c: est le caracter
+ *Return: 1 si il est en min, 0 autre
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num + '0');
+		_putchar('\n');
+	}
+	
+}

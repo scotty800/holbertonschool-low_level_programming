8893b147d64c3c079aff635cb4205959b390b239
s <s>
2024-06-24 12:12:16 +0000

    test11

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *mul -verifi si c est en min si oui return 1
+ * @a: variable int
+ * @b: variable int
+ *Return: le resultat mul
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

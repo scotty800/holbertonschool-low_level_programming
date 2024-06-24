f85d456c9b1da9bced8f84dc11dcb6345822f838
s <s>
2024-06-24 11:37:44 +0000

    test11

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isdigit -verifi si c est en min si oui return 1
+ *@c: est le caracter
+ *Return: 1 si il est en min, 0 autre
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+		return (0);
+
+}

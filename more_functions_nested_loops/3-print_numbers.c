2d3849da6b105229576f971ce6c693ad837eabe2
s <s>
2024-06-24 12:32:37 +0000

    test11

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _islower -verifi si c est en min si oui return 1
@@ -12,7 +13,7 @@ void print_numbers(void)
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		_putchar(num);
+		_putchar(num + '0');
 		_putchar('\n');
 	}
 	

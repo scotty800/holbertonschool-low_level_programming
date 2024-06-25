e652812452653ba7865875307846b01211599d0c
Your Name <you@example.com>
2024-06-25 19:22:49 +0000

    test11

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,19 @@ int main(void)
 	{
 		if (nombre == 100)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		if (nombre % 3 == 0 && nombre % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if (nombre % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (nombre % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else if (nombre <= 9)
 		{

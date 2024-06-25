97bdaf78b993a197f99c79f28cb9ffd9e297bb9c
Your Name <you@example.com>
2024-06-25 16:56:46 +0000

    test11

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square -print
+ * @size: is the variable n
+ */
+
+void print_square(int size)
+{
+	int square1, square2;
+
+	for (square1 = 0; square1 < size; square1++)
+	{
+		for (square2 = 0; square2 < square1; square2++)
+		{
+			_putchar('\n');
+		}
+
+		_putchar('#');
+		_putchar('\n');
+	}
+}

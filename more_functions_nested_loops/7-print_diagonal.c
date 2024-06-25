925f970d184b9793c7ad9be0611a271ea8af04ec
root <you@example.com>
2024-06-25 08:47:11 +0000

    test13

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal -print.
+ * @n: is the variable n
+ */
+
+void print_diagonal(int n)
+{
+	int size, nix;
+
+	if (n <= 0)
+	{
+		for (size = 0; size < n; size++)
+		{
+			for (nix = 0; nix < size; nix++)
+			{
+				_putchar(' ');
+			}
+		}
+		
+		_putchar('\\');
+		_putchar('\n');
+	}
+}

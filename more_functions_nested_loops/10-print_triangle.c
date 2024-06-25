09ef713a13addfc67358962ec0a63498f7838cf3
Your Name <you@example.com>
2024-06-25 20:47:51 +0000

    test11

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,25 +8,23 @@
 
 void print_triangle(int size)
 {
-	int triangle1, triangle2;
+	int triangle1, triangle2, tri3;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 
-	for (triangle1 = 0; triangle1 <= size; triangle1++)
+	for (triangle1 = 1; triangle1 <= size; triangle1++)
 	{
-		for (triangle2 = 0; triangle2 <= triangle1; triangle2++)
+		for (triangle2 = 1; triangle2 <= size - triangle1; triangle2++)
 		{
-			if (triangle2 > 0 && triangle2 < (triangle1 -1) && triangle1 > 0 && triangle1 < size)
-			{
-				_putchar(' ');
-			}
-			else
-				_putchar('#');
+			_putchar(' ');
 		}
-		_putchar(' ');
+		for (tri3 = 1; tri3 <= triangle1; tri3++)
+		{
+			_putchar('#');
+		}
 		_putchar('\n');
 	}
 }

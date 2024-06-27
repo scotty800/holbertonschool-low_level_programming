fa337cfd88d8b24aff3fbca7822fd9fd826b379f
Your Name <you@example.com>
2024-06-27 22:11:01 +0000

    test3

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,7 +12,7 @@ int _atoi(char *s)
 {
 	int i = 0;
 	int result = 0;
-	int sign = 1;
+	int sign = -1;
 	int boolen = 0;
 
 	while (s[i] != '\0')
@@ -30,7 +30,7 @@ int _atoi(char *s)
 			boolen = 1;
 			result = result * 10 + (s[i] - '0');
 		}
-		else if (boolen)
+		else if (boolen == 1)
 		{
 			break;
 		}

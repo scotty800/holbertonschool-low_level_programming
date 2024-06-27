959ccf9027700adcdb87e987ae144dfefa6f66c9
Your Name <you@example.com>
2024-06-27 12:27:01 +0000

    test

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts_half -print
+ * @str: is the variable
+ * Return:
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int m, x;
+
+	while (str[len] != '\0')
+	{
+	}	
+	
+	if (len % 2  == 0)
+	{
+		m = len / 2;
+	}
+	else 
+	{
+		m = (len - 1) /2 +1;
+	}
+
+	for (x = m; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}
+
+	_putchar('\n');
+}

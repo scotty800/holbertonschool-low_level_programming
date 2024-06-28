e94b1f63800e0dbc0302621a39839811e5fc6dbd
Your Name <you@example.com>
2024-06-28 11:08:35 +0000

    test14

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -14,20 +14,20 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int s = 0;
 
-	while (s1[i] != 0)
-	{
-		i++;
-	}
-
 	while (s2[s] != 0)
 	{
 		s++;
 	}
 
-	if(s1[i] == s2[s])
+	while (s1[i] != 0)
 	{
-		return (1);
+		i++;
+	}
+
+	if (s1[i] == s2[s])
+	{
+		return (0);
 	}
 	else
-		return (0);
+		return (s1[i] - s2[s]);
 }

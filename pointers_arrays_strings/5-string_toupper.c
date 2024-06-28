49787915e133a4347dff6f555923c18872c392bb
Your Name <you@example.com>
2024-06-28 19:03:45 +0000

    test15

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * *string_toupper -print
+ * @str: pointer
+ * Return: i lowercase to uppercase
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 32;
+		}
+
+	}
+
+	return (str);
+}

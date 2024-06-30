dedf06d2df5c21a2feb2c20b05622880613f6cd0
Your Name <you@example.com>
2024-06-30 15:23:59 +0000

    test

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,11 +14,11 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0 || str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-		str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-		str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-		str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-		str[i] == '}')
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+		str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+		str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+		str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+		str[i - 1] == '}')
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{

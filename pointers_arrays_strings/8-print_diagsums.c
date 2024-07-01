d0e8c52f93279a291b63bb1b678df37ba96f7799
Your Name <you@example.com>
2024-07-01 20:22:45 +0000

    The line of life is a ragged diagonal between duty and desire

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -3,13 +3,31 @@
 #include <stdio.h>
 
 /**
- * *_memset -priny that encodes a string
- * @s: pointer
- * @b: caract
- * @n: octet
- * Return: s
+ * print_diagsums -priny that encodes a string
+ * @a: pointer
+ * @size: octet
  */
 void print_diagsums(int *a, int size)
 {
+	int i;
 
+	int diag1 = 0;
+	int diag2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		diag1 += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		diag2 += a[i];
+		a -= size;
+	}
+	
+
+	printf("%d, %d\n", diag1, diag2);
 }

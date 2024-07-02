8650f696ac528ad3fdb4d33cbe6efabc7f8258e8
Your Name <you@example.com>
2024-07-02 20:45:31 +0000

    exo

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -12,4 +12,6 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\0');
 }
--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -11,13 +11,18 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y <= 0)
+	if (y == 0)
 	{
 		return (1);
 	}
-	if (x == 0)
+	if (y == 1)
 	{
-		return (0);
+		return (x);
 	}
-	return (x * _pow_recursion(x, y - 1));
+	if (y > 1)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
+		return (-1);
 }

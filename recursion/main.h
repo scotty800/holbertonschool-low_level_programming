532b8edbb8600e39321859b48911f25e5ab3f7e0
Your Name <you@example.com>
2024-07-03 12:02:09 +0000

    test

--- recursion/main.h
+++ recursion/main.h
@@ -10,4 +10,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _is_prime_i(int a, int i);
 #endif

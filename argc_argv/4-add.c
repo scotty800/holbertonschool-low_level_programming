303066ea817860e066bb39d4e11e8f5992e430f3
Your Name <you@example.com>
2024-07-04 14:48:36 +0000

    test3

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -program that prints its name
+ * @argc: variable
+ * @argv: pointeur
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int somme;
+	int i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; j < argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			somme = somme + atoi(argv[i]);
+		}
+	}
+
+	printf("%d", somme);
+	return (0);
+
+}

70780cefd8468a04468a981abb4e568de02ea86e
Your Name <you@example.com>
2024-07-04 09:44:27 +0000

    test

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -22,8 +22,7 @@ int main(int argc, char **argv)
 	i = atoi(argv[1]);
 	j = atoi(argv[2]);
 	somme = (i * j);
-	
-		printf("%d\n", somme);
-		return (0);
-	
+
+	printf("%d\n", somme);
+	return (0);
 }

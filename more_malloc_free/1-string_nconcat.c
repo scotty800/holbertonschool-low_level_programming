c948c9ae72de01081aa9d52cc54f3cff3a85e083
Your Name <you@example.com>
2024-07-10 09:17:33 +0000

    Write a function that concatenates two strings.

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,9 +12,9 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int index, index2;
-	int len1, len2;
-	int oct = n;
+	unsigned int index, index2;
+	unsigned int len1, len2;
+	unsigned int oct = n;
 	char *ptr;
 
 	if (s1 == NULL)

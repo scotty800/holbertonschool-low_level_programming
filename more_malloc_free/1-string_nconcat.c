fc8bae08b5a01bafdd6215163e01b6b34bc0444d
Your Name <you@example.com>
2024-07-10 08:51:55 +0000

    Write a function that concatenates two strings.

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *string_nconcat -concatene deux chaines en utilisant n octets
+ * @s1: la premier chaine de carateres
+ * @s2: la seconde chaine de caracyres
+ * @n: le nombre d'octets de s2 a concatener a s1
+ * Return: ptr
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int index, index2;
+	int len1, len2;
+	int oct = n;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 = " ";
+
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+
+	if (oct >= len2)
+		ptr = malloc(sizeof(char) * (len2 + 1));
+	else
+		ptr = malloc(sizeof(char) * (len1 + n + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (index = 0; s1[index] != '\0'; index++)
+	{
+		ptr[index] = s1[index];
+	}
+
+	for (index2 = 0; s2[index2] != '\0'; index2++)
+	{
+		ptr[index + index2] = s2[index2];
+	}
+
+	ptr[index + index2] = '\0';
+
+	return (ptr);
+}

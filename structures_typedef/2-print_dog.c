2ef99b4e3b30a017e3234d7acf56283c8fa72649
Your Name <you@example.com>
2024-07-10 18:25:34 +0000

    Write a function that prints a struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("(nil)\n");
+		printf(" name : (nil)\n");
 	}
 	else
 	{
@@ -22,7 +22,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->age < 0)
 	{
-		printf("(nil)\n");
+		printf("age : (nil)\n");
 	}
 	else
 	{
@@ -30,7 +30,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->owner == NULL)
 	{
-		printf("(nil)\n");
+		printf("owner : (nil)\n");
 	}
 	else
 	{

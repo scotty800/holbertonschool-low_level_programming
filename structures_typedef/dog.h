06a92a0101f94cc76adc84f96548bd71e96b8f2d
Your Name <you@example.com>
2024-07-10 16:39:29 +0000

    Define a new type struct dog with the following elements:

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,4 +8,9 @@ typedef struct dog
 	char *owner;
 } my_dog;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+my_dog *new_dog(char *name, float age, char *owner);
+void free_dog(my_dog *d);
+
 #endif

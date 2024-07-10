b25d37bbc7499cd740a8f21eea31f6273e6edf9e
Your Name <you@example.com>
2024-07-10 17:38:34 +0000

    important

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,11 +8,13 @@
  * @owner: the ownerof the dog
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} my_dog
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif

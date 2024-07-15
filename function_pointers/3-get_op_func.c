69f80cdc8097340845295ea663db75f050dca767
Your Name <you@example.com>
2024-07-15 15:58:12 +0000

    just for now

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * print_name -Imprme un nom en utilisant une fonction de rappel
+ * @name: le nom a imprimer
+ * @f: pointeur vers la functiob de rappel utilisee pour imprimer le nom
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (ops[i].op != NULL)
+	{
+		if (s(ops[i]).f)
+			return (i);
+	}
+
+	return (NULL);
+
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add -Ajoute deux entiers
+ * @a: le premier entier
+ * @b: le deuxieme entier
+sh: 1: q: not found
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub -Ajoute deux entiers
+ * @a: le premier entier
+ * @b: le deuxieme entier
+ * Return: le resultat
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul -Ajoute deux entiers
+ * @a: le premier entier
+ * @b: le deuxieme entier
+ * Return: le resultat
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div -Ajoute deux entiers
+ * @a: le premier entier
+ * @b: le deuxieme entier
+ * Return: le resultat
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod -Ajoute deux entiers
+ * @a: le premier entier
+ * @b: le deuxieme entier
+ * Return: le resultat
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

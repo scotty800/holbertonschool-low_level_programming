2c447c0f0f181c1b766365f60ac25491c6ad8f22
Your Name <you@example.com>
2024-07-15 14:35:02 +0000

    Write a function that searches for an integer.

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,10 +1,12 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * int_index -recherche un entier dans un tableau en utilisant une fonction de comparaison
+ * int_index -recherche un entier dans un tableau en utilisant
+ * une fonction de comparaison
  * @array: le tableau d'entiers
  * @size: la taille du tableau
  * @cmp: pointeur vers la fonction de comparaison
+ * Return: -1 si c'est faut
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -12,12 +14,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
-	
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-		return (i);
+			return (i);
 	}
-		
+
 	return (-1);
 }

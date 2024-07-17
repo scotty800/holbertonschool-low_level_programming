af091ba5eb5711d07ddf1cc803a36f2dee7e4ea4
Your Name <you@example.com>
2024-07-17 15:08:02 +0000

    Write a function that prints anything.

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,93 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_char - Prints a character
+ * @ap: The va_list containing the character to print
+ */
+
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_integer - Prints an integer
+ * @ap: The va_list containing the integer to print
+ */
+
+void print_integer(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * print_float - Prints a float
+ * @ap: The va_list containing the float to print
+ */
+
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_string - Prints a string
+ * @ap: The va_list containing the string to print
+ */
+
+void print_string(va_list ap)
+{
+	char *s = va_arg(ap, char*);
+
+	if (s == NULL)
+	{
+		s = "(nil)";
+	}
+	else
+		printf("%s", s);
+}
+
+/**
+ * print_all - Prints anything according to the given format
+ * @format: A list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	sp_t spt[] = {
+		{'c', print_char},
+		{'i', print_integer},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL},
+	};
+
+	va_list ap;
+	int index = 0, j;
+	char *separator = "";
+
+	va_start(ap, format);
+
+	while (format && format[index])
+	{
+		j = 0;
+		while (spt[j].specifi)
+		{
+			if (format[index] == spt[j].specifi)
+			{
+				printf("%s", separator);
+				spt[j].print_func(ap);
+				separator = " , ";
+				break;
+			}
+			j++;
+		}
+		index++;
+	}
+	va_end(ap);
+	printf("\n");
+
+
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,25 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+/**
+ * struct format_sp - Struct for format specifiers and corresponding functions
+ * @specifi: The format specifier character
+ * @print_func: The function to call for printing the specified type
+ */
+
+typedef struct format_sp
+{
+	char specifi;
+	void (*print_func)(va_list ap);
+} sp_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_char(va_list ap);
+void print_integer(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
 #endif

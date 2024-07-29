dd93e00513b0edc7dc380da7c3ae739f32806bc5
root <root@DESKTOP-SH55I7R>
2024-07-29 14:45:38 +0200

    correction betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,18 +1,24 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: Pointer to the head of the list
+ * Return: The number of nodes in the list
+ */
+
 size_t print_list(const list_t *h)
 {
 	int size = 0;
-	
-	while(h != NULL)
+
+	while (h != NULL)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
-			printf("[%u] %s\n", h->len, h->str); 
+			printf("[%u] %s\n", h->len, h->str);
 		++size;
 		h = h->next;
 	}

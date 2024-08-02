da179700e5edcd6543db60a9fb5441c3ff0e1ba5
root <root@DESKTOP-SH55I7R>
2024-08-02 15:37:06 +0200

    Corretion betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: nime of the file
+ * @letters: number of letters to print
+ * Return: 0 if filename NULL
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
@@ -29,8 +36,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (file != -1)
 	{
-		 read_file = read(file, buffer, letters);
-		 write_file = write(STDOUT_FILENO, buffer, read_file);
+		read_file = read(file, buffer, letters);
+		write_file = write(STDOUT_FILENO, buffer, read_file);
 	}
 	close(file);
 	free(buffer);
